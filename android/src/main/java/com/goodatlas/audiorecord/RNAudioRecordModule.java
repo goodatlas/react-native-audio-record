803dd62054ef9df6064fd4785343c4ab0c70451d
Max Kelly <maxckel@gmail.com>
2021-09-09 14:06:40 +1000

    Removed file save and increased SDK min version

--- android/src/main/java/com/goodatlas/audiorecord/RNAudioRecordModule.java
+++ android/src/main/java/com/goodatlas/audiorecord/RNAudioRecordModule.java
@@ -31,12 +31,6 @@ public class RNAudioRecordModule extends ReactContextBaseJavaModule {
     private AudioRecord recorder;
     private int bufferSize;
     private boolean isRecording;
-    private boolean saveWavBoolean;
-
-    private String tmpFile;
-    private String outFile;
-    private Promise stopRecordingPromise;
-
 
     public RNAudioRecordModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -69,23 +63,9 @@ public class RNAudioRecordModule extends ReactContextBaseJavaModule {
             }
         }
 
-        audioSource = AudioSource.VOICE_RECOGNITION;
+        audioSource = AudioSource.VOICE_COMMUNICATION;
         if (options.hasKey("audioSource")) {
             audioSource = options.getInt("audioSource");
-        }
-
-        if (options.hasKey("saveWavBoolean")) {
-            saveWavBoolean = options.getBoolean("saveWavBoolean");
-        } else {
-            saveWavBoolean = false;
-        }
-
-        String documentDirectoryPath = getReactApplicationContext().getFilesDir().getAbsolutePath();
-        outFile = documentDirectoryPath + "/" + "audio.wav";
-        tmpFile = documentDirectoryPath + "/" + "temp.pcm";
-        if (options.hasKey("wavFile")) {
-            String fileName = options.getString("wavFile");
-            outFile = documentDirectoryPath + "/" + fileName;
         }
 
         isRecording = false;
@@ -109,8 +89,6 @@ public class RNAudioRecordModule extends ReactContextBaseJavaModule {
                     int count = 0;
                     String base64Data;
                     byte[] buffer = new byte[bufferSize];
-                    FileOutputStream os = new FileOutputStream(tmpFile);
-
                     while (isRecording) {
                         bytesRead = recorder.read(buffer, 0, buffer.length);
 
@@ -118,16 +96,10 @@ public class RNAudioRecordModule extends ReactContextBaseJavaModule {
                         if (bytesRead > 0 && ++count > 2) {
                             base64Data = Base64.encodeToString(buffer, Base64.NO_WRAP);
                             eventEmitter.emit("data", base64Data);
-                            os.write(buffer, 0, bytesRead);
                         }
                     }
 
                     recorder.stop();
-                    os.close();
-                    if (saveWavBoolean) {
-                        saveAsWav();
-                    }
-                    stopRecordingPromise.resolve(outFile);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -138,97 +110,8 @@ public class RNAudioRecordModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void stop(Promise promise) {
+    public void stop() {
         isRecording = false;
-        stopRecordingPromise = promise;
-    }
-
-    private void saveAsWav() {
-        try {
-            FileInputStream in = new FileInputStream(tmpFile);
-            FileOutputStream out = new FileOutputStream(outFile);
-            long totalAudioLen = in.getChannel().size();;
-            long totalDataLen = totalAudioLen + 36;
-
-            addWavHeader(out, totalAudioLen, totalDataLen);
-
-            byte[] data = new byte[bufferSize];
-            int bytesRead;
-            while ((bytesRead = in.read(data)) != -1) {
-                out.write(data, 0, bytesRead);
-            }
-            Log.d(TAG, "file path:" + outFile);
-            Log.d(TAG, "file size:" + out.getChannel().size());
-
-            in.close();
-            out.close();
-            deleteTempFile();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void addWavHeader(FileOutputStream out, long totalAudioLen, long totalDataLen)
-            throws Exception {
-
-        long sampleRate = sampleRateInHz;
-        int channels = channelConfig == AudioFormat.CHANNEL_IN_MONO ? 1 : 2;
-        int bitsPerSample = audioFormat == AudioFormat.ENCODING_PCM_8BIT ? 8 : 16;
-        long byteRate =  sampleRate * channels * bitsPerSample / 8;
-        int blockAlign = channels * bitsPerSample / 8;
-
-        byte[] header = new byte[44];
-
-        header[0] = 'R';                                    // RIFF chunk
-        header[1] = 'I';
-        header[2] = 'F';
-        header[3] = 'F';
-        header[4] = (byte) (totalDataLen & 0xff);           // how big is the rest of this file
-        header[5] = (byte) ((totalDataLen >> 8) & 0xff);
-        header[6] = (byte) ((totalDataLen >> 16) & 0xff);
-        header[7] = (byte) ((totalDataLen >> 24) & 0xff);
-        header[8] = 'W';                                    // WAVE chunk
-        header[9] = 'A';
-        header[10] = 'V';
-        header[11] = 'E';
-        header[12] = 'f';                                   // 'fmt ' chunk
-        header[13] = 'm';
-        header[14] = 't';
-        header[15] = ' ';
-        header[16] = 16;                                    // 4 bytes: size of 'fmt ' chunk
-        header[17] = 0;
-        header[18] = 0;
-        header[19] = 0;
-        header[20] = 1;                                     // format = 1 for PCM
-        header[21] = 0;
-        header[22] = (byte) channels;                       // mono or stereo
-        header[23] = 0;
-        header[24] = (byte) (sampleRate & 0xff);            // samples per second
-        header[25] = (byte) ((sampleRate >> 8) & 0xff);
-        header[26] = (byte) ((sampleRate >> 16) & 0xff);
-        header[27] = (byte) ((sampleRate >> 24) & 0xff);
-        header[28] = (byte) (byteRate & 0xff);              // bytes per second
-        header[29] = (byte) ((byteRate >> 8) & 0xff);
-        header[30] = (byte) ((byteRate >> 16) & 0xff);
-        header[31] = (byte) ((byteRate >> 24) & 0xff);
-        header[32] = (byte) blockAlign;                     // bytes in one sample, for all channels
-        header[33] = 0;
-        header[34] = (byte) bitsPerSample;                  // bits in a sample
-        header[35] = 0;
-        header[36] = 'd';                                   // beginning of the data chunk
-        header[37] = 'a';
-        header[38] = 't';
-        header[39] = 'a';
-        header[40] = (byte) (totalAudioLen & 0xff);         // how big is this data chunk
-        header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
-        header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
-        header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
-
-        out.write(header, 0, 44);
-    }
-
-    private void deleteTempFile() {
-        File file = new File(tmpFile);
-        file.delete();
     }
 }
+
